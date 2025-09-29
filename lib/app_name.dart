50c1017f1519ef526516595dd0d9b14baa326963
hot4ixa <ggdfasolk@gmail.com>
2025-09-29 23:19:09 +0300

    где-то накосячил

---
+++ lib/app_name.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/widgets.dart';
+
+class AppName extends StatelessWidget {
+  const AppName({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
+      title: 'Название приложения',
+      theme: AppTheme.lightTheme,
+      routeInformationProvider: router.routeInformationProvider,
+      routeInformationParser: router.routeInformationParser,
+      routerDelegate: router.routerDelegate,
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,11 @@
-import 'package:flutter/material.dart';
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  setupLocator();
 
-void main() {
-  runApp(const MainApp());
+  FlutterError.onError = (details) {
+    return talker.handle(details.exception, details.stack);
+  };
+
+  runApp(AppName());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
-  }
-}
